f5177a26d7844494c20f103936a4c29f446c4eb6
Ilya Danilov <esozap32@mail.ru>
2022-10-30 21:48:16 +0400

    Glass

---
+++ src/main/java/Glass.java
@@ -0,0 +1,8 @@
+public class Glass {
+    public static void main(String[] args) {
+        int glassVolume = 250;
+        double persent = 0.33;
+        double calculated = (glassVolume * persent);
+        System.out.print("В 33 процентах содержится: " + calculated + (" мл."));
+    }
+}
